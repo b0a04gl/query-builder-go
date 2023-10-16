fa896724c3f1fa5a8606257a77910401b6158fac
balaji6666 <balaji.foo404@gmail.com>
2023-10-16 21:36:49 +0530

    updated db interaction

--- database/database.go
+++ database/database.go
@@ -3,8 +3,10 @@ package database
 import (
 	"database/sql"
 	"fmt"
-	_ "github.com/lib/pq"
 	"log"
+
+	_ "github.com/lib/pq"
+	"github.com/querybuilder/model"
 )
 
 type DB struct {
@@ -32,33 +34,31 @@ func Get(connStr string) {
 	fmt.Printf("connected to %v\n\n", GlobalDB.Client)
 }
 
-func Read(query string,args any) {
+func Read(query string, args any) ([]model.Player){
 
-	if GlobalDB == nil{
+	if GlobalDB == nil {
 		log.Fatal("DB not available")
 	}
 
-	rows, err := GlobalDB.Client.Query(query,args)
+	rows, err := GlobalDB.Client.Query(query, args)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer rows.Close()
 
+	var players []model.Player
+
 	for rows.Next() {
-		var id int
-		var name string
-		var team string
-		var role string
-		var total_runs int
-		var total_wickets int
-		if err := rows.Scan(&id, &name, &team, &role, &total_runs, &total_wickets); err != nil {
+		var player model.Player
+		if err := rows.Scan(&player.Id, &player.Name, &player.Team, &player.Role, &player.Total_runs, &player.Total_wickets); err != nil {
 			log.Fatal(err)
 		}
-		fmt.Printf("\nID: %d, Name: %s, team: %s, role: %s, total_runs: %d, total_wickets : %d \n", id, name, team, role, total_runs, total_wickets)
-
+		players = append(players, player)
 	}
 
 	if err := rows.Err(); err != nil {
 		log.Fatal(err)
 	}
+
+	return players
 }
